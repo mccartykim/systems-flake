749a8c5990efa6213676b0787444b95873ba2f5e
Kimberly McCarty <mccartykim@zoho.com>
2024-05-16 23:22:21 -0400

    added dep

--- flake.nix
+++ flake.nix
@@ -54,13 +54,18 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
-        modules = [./home/marshmallow.nix];
+        modules = [./home/marshmallow.nix
+	nixvim/nixosModules.nixvim
+	];
       };
       "kimb@total-eclipse" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
-        modules = [./home/total-eclipse.nix];
+        modules = [
+	nixvim.nixosModules.nixvim
+	./home/total-eclipse.nix
+	];
       };
     };
   };
