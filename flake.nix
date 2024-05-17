356194dd2f47bf922658b99c63b72fc38f8248f4
Kimberly McCarty <mccartykim@zoho.com>
2024-05-16 23:14:47 -0400

    oh right, home-manager

--- flake.nix
+++ flake.nix
@@ -48,7 +48,13 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+        modules = [./home/marshmallow.nix];
+      };
+      "kimb@total-eclipse" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [./home/total-eclipse.nix];
       };
     };
   };
