ba110b2edd36147719a2b8f763b4e02360ea6d57
Kimb <mccartykim@zoho.com>
2024-05-19 00:03:18 -0400

    updated reference to devshell

--- flake.nix
+++ flake.nix
@@ -101,5 +101,12 @@
         ];
       };
     };
+    devShell.x86_64-linux = let
+      system = "x86_64-linux";
+      pkgs  = import nixpkgs { inherit system; };
+    in
+    pkgs.mkShell {
+      packages = [ pkgs.tealdeer ];
+    };
   };
 }
