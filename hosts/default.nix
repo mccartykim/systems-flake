0a74bbbebd8c5e25004411c986bd2c4d5b4f51ad
Kimb <mccartykim@zoho.com>
2024-05-21 19:17:55 -0400

    Addressed error on build

--- hosts/default.nix
+++ hosts/default.nix
@@ -25,6 +25,7 @@
   # Enable networking
   networking.networkmanager.enable = true;
   xdg.portal.enable = true;
+  xdg.portal.extraPortals = pkgs.xdg-desktop-portal-kde;
 
   # Set your time zone.
   time.timeZone = "America/New_York";
