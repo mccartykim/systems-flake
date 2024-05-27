a57ac89522e9204794fe9360bcefd81917d4e910
Kimberly McCarty <mccartykim@zoho.com>
2024-05-26 21:51:17 -0400

    Trying reddit suggestion to tweak sunshine, still prob needs config

--- hosts/total-eclipse/configuration.nix
+++ hosts/total-eclipse/configuration.nix
@@ -104,6 +104,25 @@
 
   programs.fish.enable = true;
 
+  security.wrappers.sunshine = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_admin+p";
+    source = "${pkgs.sunshine}/bin/sunshine";
+  };
+
+  services.avahi.publish.enable = true;
+  services.avahi.publish.userServices = true;
+
+  networking.firewall = {
+  enable = true;
+  allowedTCPPorts = [ 47984 47989 47990 48010 ];
+  allowedUDPPortRanges = [
+    { from = 47998; to = 48000; }
+    #{ from = 8000; to = 8010; }
+    ];
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
