129ef62162d40e4688a0cce0bb6313396d44c6e0
Kimb <mccartykim@zoho.com>
2024-07-17 01:41:41 -0400

    I think I have it mostly good

--- home/marshmallow.nix
+++ home/marshmallow.nix
@@ -80,9 +80,85 @@
 
   wayland.windowManager.sway.enable = true;
   wayland.windowManager.sway.config.modifier = "Mod4";
-  programs.waybar.enable=false;
+  wayland.windowManager.sway.config = {
+    menu = "wofi --show drun,run";
+  };
+  programs.swayr.enable = true;
+  programs.swayr.settings = {
+    menu = {
+      executable = "${pkgs.wofi}/bin/wofi";
+      args = [
+	"--show=dmenu"
+	"--allow-markup"
+	"--allow-images"
+	"--insensitive"
+	"--cache-file=/dev/null"
+	"--parse-search"
+	"--height=40%"
+	"--prompt={prompt}"
+      ];
+    };
+
+    format = {
+      output_format = "{indent}<b>Output {name}</b> <span alpha='\"20000\"'>({id})</span>";
+      workspace_format = "{indent}<b>Workspace {name} [{layout}]</b> on output {output_name}    <span alpha='\"20000\"'>({id})</span>";
+      container_format = "{indent}<b>Container [{layout}]</b> <i>{marks}</i> on workspace {workspace_name}    <span alpha='\"20000\"'>({id})</span>";
+      window_format = "img:{app_icon}:text:{indent}<i>{app_name}</i> — {urgency_start}<b>“{title}”</b>{urgency_end} <i>{marks}</i> on workspace {workspace_name} / {output_name}    <span alpha='\"20000\"'>({id})</span>";
+      indent = "    ";
+      urgency_start = "<span background='\"darkred\"' foreground='\"yellow\"'>";
+      urgency_end = "</span>";
+      html_escape = true;
+    };
+
+    layout = {
+      auto_tile = false;
+      auto_tile_min_window_width_per_output_width = [
+	[ 800 400 ]
+	[ 1024 500 ]
+	[ 1280 600 ]
+	[ 1400 680 ]
+	[ 1440 700 ]
+	[ 1600 780 ]
+	[ 1680 780 ]
+	[ 1920 920 ]
+	[ 2048 980 ]
+	[ 2560 1000 ]
+	[ 3440 1200 ]
+	[ 3840 1280 ]
+	[ 4096 1400 ]
+	[ 4480 1600 ]
+	[ 7680 2400 ]
+      ];
+    };
+
+    focus = {
+      lockin_delay = 750;
+    };
+
+    misc = {
+      seq_inhibit = false;
+    };
+  };
+  programs.waybar.enable= true;
   programs.waybar.systemd.enable = true;
-  programs.wofi.enable = true;
+  programs.wofi = { 
+    enable = true;
+    settings = {
+      allow_images = true;
+      insensitive = true;
+      allow_markup = true;
+    };
+    style = ''
+      #entry {  
+	padding: 0px;  
+	margin: 0px;              
+	border-bottom-style: solid;  
+	border-width: 2px;                                  
+	border-color: @base02;  
+	background-color: @base00;
+      }
+    '';
+  };
 
   services.swayidle.enable = true;
 
