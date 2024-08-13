e160ce797760d8c7da8a00b011ab7dada0060b60
Kimberly McCarty <kimberly.mccarty@onepeloton.com>
2024-08-13 16:58:17 -0400

    Bumped lix version, let's gooo

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     stylix.url = "github:danth/stylix";
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin.url = "github:LnL7/nix-darwin";
