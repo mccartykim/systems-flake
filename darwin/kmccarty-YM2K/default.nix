08e654e2b5191859feff6a7ace8c080728763602
Kimb <mccartykim@zoho.com>
2024-09-16 21:40:54 -0400

    Maybe this hash is it?

--- darwin/kmccarty-YM2K/default.nix
+++ darwin/kmccarty-YM2K/default.nix
@@ -10,7 +10,7 @@
   corretto17 = pkgs.fetchzip {
     name = "corretto17";
     url = "https://corretto.aws/downloads/latest/amazon-corretto-17-aarch64-macos-jdk.tar.gz";
-    hash = "1ahi1zkd8kgzq1ksgwwn7pw59a9mppgdsw6z4j6i7r1vna0s8rf1";
+    hash = "0jqm55w8d1vqfkxkimp2ma1x4vs06xdwkqmmirrygmzb2shi9k3p";
   };
 in {
   # DO NOT SET ENVIRONMENTAL VARIABLES ELSEWHERE
