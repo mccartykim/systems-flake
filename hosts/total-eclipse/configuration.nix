5655840cedbf118dc437016ec21d0407d28892ee
Kimberly McCarty <mccartykim@zoho.com>
2024-09-25 23:34:12 -0400

    wifi missing?

--- hosts/total-eclipse/configuration.nix
+++ hosts/total-eclipse/configuration.nix
@@ -42,7 +42,7 @@
   };
 
   networking.hostName = "total-eclipse"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
