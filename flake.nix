d951e9f2c5156918242c3bdf9f702a86b6ff3948
Kimberly McCarty <kimberly.mccarty@onepeloton.com>
2024-10-16 12:55:09 -0400

    Let's see if we can fix nix-darwin with lix disabled

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
         home-manager.darwinModules.home-manager
         ./darwin/kmccarty-YM2K/default.nix
         ./home/work-laptop.nix
-        lix-module.nixosModules.default
+        # lix-module.nixosModules.default
       ];
     };
 
