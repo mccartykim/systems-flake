8b91c70ae507b9d8a13cbc9c5e23cc6c57c2ea9a
Kimberly McCarty <kimberly.mccarty@onepeloton.com>
2024-10-16 12:03:32 -0400

    Removed unneeded home-manager config, I'm a messy freak who just switches the whole os at once

--- flake.nix
+++ flake.nix
@@ -118,28 +118,6 @@
       };
     };
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      "kimb@marshmallow" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [
-          ./home/marshmallow.nix
-          stylix.nixosModules.stylix
-        ];
-      };
-      "kimb@total-eclipse" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [
-          ./home/total-eclipse.nix
-          stylix.nixosModules.stylix
-        ];
-      };
-    };
     devShell.x86_64-linux = let
       system = "x86_64-linux";
       pkgs = import nixpkgs {inherit system;};
