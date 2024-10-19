d7056ac431e5c98f3676b9d8db9d6486f78033d9
Kimberly McCarty <kimb@kimb.dev>
2024-10-19 19:45:51 -0400

    Going to set a consistent theme because this autotheming is getting tedious!

--- hosts/marshmallow/style/default.nix
+++ hosts/marshmallow/style/default.nix
@@ -6,7 +6,7 @@
 }: {
   stylix.enable = true;
   stylix.image = ./marsh-flower.jpg;
-  stylix.polarity = "dark";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-light-medium.yaml";
   stylix.fonts = {
     sizes = {
       desktop = 14;
