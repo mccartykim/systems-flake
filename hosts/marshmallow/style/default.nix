10d0438c654a1cca8e3949378c86bbf82c204162
Kimberly McCarty <kimb@kimb.dev>
2024-10-22 23:52:16 -0400

    Switched to sakura theme, looks a bit better?

--- hosts/marshmallow/style/default.nix
+++ hosts/marshmallow/style/default.nix
@@ -6,7 +6,7 @@
 }: {
   stylix.enable = true;
   stylix.image = ./marsh-flower.jpg;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-light-medium.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/sakura.yaml";
   stylix.fonts = {
     sizes = {
       desktop = 14;
