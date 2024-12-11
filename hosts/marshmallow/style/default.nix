8031a058517eaf00c668e39abe691dcf9a4a5876
Kimberly McCarty <kimb@kimb.dev>
2024-12-10 22:51:50 -0500

    monokai

--- hosts/marshmallow/style/default.nix
+++ hosts/marshmallow/style/default.nix
@@ -6,7 +6,7 @@
 }: {
   stylix.enable = true;
   stylix.image = ./marsh-flower.jpg;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/sakura.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/monokai.yaml";
   stylix.fonts = {
     sizes = {
       desktop = 14;
