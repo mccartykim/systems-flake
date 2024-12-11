affc40e63bbe29d27980c49d7abb8b12d3c15c0f
Kimberly McCarty <mccartykim@zoho.com>
2024-12-10 21:34:27 -0500

    Upgrading to plasma 6

--- hosts/total-eclipse/configuration.nix
+++ hosts/total-eclipse/configuration.nix
@@ -72,8 +72,7 @@
   services.xserver.enable = true;
 
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-  services.xserver.desktopManager.plasma5.bigscreen.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
