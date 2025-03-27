fc12353e6e6abeafd6f40ef2002f163961974470
Kimberly McCarty <kimb@kimb.dev>
2025-03-27 04:09:37 -0400

    refactor(total-eclipse): remove Steam and Hyprland from systemPackages

--- hosts/total-eclipse/configuration.nix
+++ hosts/total-eclipse/configuration.nix
@@ -124,8 +124,6 @@
     pkgs.neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     pkgs.wget
     pkgs.git
-    pkgs.hyprland
-    pkgs.steam
     pkgs.legendary-gl
     pkgs.sunshine
   ];
