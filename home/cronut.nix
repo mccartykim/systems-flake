a18987ded1de560a704e576425b0ac91c392a66a
Kimb <kimb@kimb.dev>
2025-04-13 20:09:11 -0400

    feat(cronut): added jj to fish vcs prompt

--- home/cronut.nix
+++ home/cronut.nix
@@ -83,6 +83,49 @@
           src = pkgs.fishPlugins.autopair.src;
         }
       ];
+      functions = {
+        fish_jj_prompt = ''
+          # Is jj installed?
+          if not command -sq jj
+              return 1
+          end
+
+          # Are we in a jj repo?
+          if not jj root --quiet --no-pager &>/dev/null
+              return 1
+          end
+
+          # Generate prompt
+          jj log --ignore-working-copy --no-pager --no-graph --color always -r @ -T '
+              surround(
+                  " (",
+                  ")",
+                  separate(
+                      " ",
+                      bookmarks.join(", "),
+                      coalesce(
+                          surround(
+                              "\"",
+                              "\"",
+                              if(
+                                  description.first_line().substr(0, 24).starts_with(description.first_line()),
+                                  description.first_line().substr(0, 24),
+                                  description.first_line().substr(0, 23) ++ "…"
+                              )
+                          ),
+                          "(no desc)"
+                      ),
+                      change_id.shortest(),
+                      commit_id.shortest(),
+                      if(conflict, "(conflict)"),
+                      if(empty, "(empty)"),
+                      if(divergent, "(divergent)"),
+                      if(hidden, "(hidden)"),
+                  )
+              )
+          '
+        '';
+      };
       # Fish path bug workaround
       shellInit = let
         # This naive quoting is good enough in this case. There shouldn't be any
