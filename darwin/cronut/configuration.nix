5a62c02112b8418c45cf68c5d7a9b3415df9f66e
Kimberly McCarty <kimberly.mccarty@onepeloton.com>
2025-04-22 12:14:24 -0400

    feat(cronut, work-laptop): added srvos

--- darwin/cronut/configuration.nix
+++ darwin/cronut/configuration.nix
@@ -28,18 +28,13 @@
   home-manager.backupFileExtension = "backup";
 
   # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
   nix.settings.trusted-users = [
     "kim"
   ];
   nix.settings.substituters = [
-    "https://cache.nixos.org/"
-    "https://nix-community.cachix.org/"
     "https://cache.garnix.io"
   ];
   nix.settings.trusted-public-keys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
 
