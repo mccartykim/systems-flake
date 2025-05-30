9744ed0710aac4b91c4fece6c6b297f8d5db0ef0
Kimberly McCarty <kimb@kimb.dev>
2025-05-30 03:08:17 -0400

    Assorted extensions to phone and other tweaks, gotta split this one

---
+++ avd/default.nix
@@ -0,0 +1,39 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, lib, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+    wget
+  ];
+  
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.trusted-public-keys = [
+    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+  ];
+  nix.settings.substituters = [
+    "https://cache.garnix.io"
+  ];
+  nix.settings.trusted-substituters = [
+    "https://cache.garnix.io"
+  ];
+
+  # Change default user
+  # avf.defaultUser = "droid";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
