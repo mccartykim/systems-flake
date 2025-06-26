92b3cff5efbf573309c3930ba82d42b6bdc24439
Kimberly McCarty <kimb@kimb.dev>
2025-06-26 02:50:55 -0400

    refactor: neovim fixup

--- home/neovim.nix
+++ home/neovim.nix
@@ -19,13 +19,14 @@
       telescope-fzf-native-nvim
       which-key-nvim
       telekasten-nvim
+      nvim-lspconfig
       markdown-preview-nvim
     ];
 
     extraPackages = with pkgs; [
-      pkgs.ripgrep
-      pkgs.fd
-      pkgs.chafa
+      ripgrep
+      fd
+      chafa
     ];
 
     extraConfig = ''
@@ -33,7 +34,7 @@
       set smartcase
       filetype plugin indent on
       set shiftwidth=2
-      syntax on " lol it can't be necessary
+      syntax on
       filetype plugin on
       set cursorline
     '';
@@ -47,22 +48,7 @@
       local ts = require("telescope")
       ts.load_extension('fzf')
       ts.load_extension('media_files')
-      require("telekasten").setup({
-        home = vim.fn.expand("~/jujutsu/zettelkasten"),
-      });
 
-      -- Most used functions
-      vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
-      vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
-      vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
-      vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
-      vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
-      vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
-      vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
-      vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
-
-      -- Call insert link automatically when we start typing a link
-      vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
     '';
   };
 }
