ff38948ea1881db5e2406ba95c851699d9ef4236
Kimberly McCarty <kimb@kimb.dev>
2025-07-01 09:14:00 -0400

    feat(historian): adding podman under docker alias

--- hosts/historian/configuration.nix
+++ hosts/historian/configuration.nix
@@ -27,13 +27,6 @@
     };
   };
 
-  # Useful other development tools
-  environment.systemPackages = with pkgs; [
-    dive # look into docker image layers
-    podman-tui # status of containers in the terminal
-    docker-compose # start group of containers for dev
-    #podman-compose # start group of containers for dev
-  ];
   programs.nix-ld.enable = true;
 
   # Host identification
