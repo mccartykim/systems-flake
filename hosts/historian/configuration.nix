7e3b54af057dd294d97ab9644a5055f88b19333f
Kimberly McCarty <kimb@kimb.dev>
2025-07-21 17:20:19 -0400

    chore: flake bump

--- hosts/historian/configuration.nix
+++ hosts/historian/configuration.nix
@@ -31,6 +31,10 @@
 
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableKvm = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.host.addNetworkInterface = false;
 
   # Host identification
   networking.hostName = "historian";
