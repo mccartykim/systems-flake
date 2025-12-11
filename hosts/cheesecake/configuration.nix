587bbcfd68a21371c4fbc2cf0893c2160f96700f
Kimberly McCarty <kimb@kimb.dev>
2025-12-10 23:07:47 -0500

    todo: added cheesecake and reenabled ssh for better configuration

--- hosts/cheesecake/configuration.nix
+++ hosts/cheesecake/configuration.nix
@@ -1,4 +1,3 @@
-{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -197,7 +196,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
