d848546d5e337113801e21fa41152bfbd19e1c11
Kimberly McCarty <kimberly.mccarty@onepeloton.com>
2026-01-20 10:57:23 -0500

    fix(work-laptop): adding shell env vars to go with gui env vars

--- darwin/kmccarty-YM2K/default.nix
+++ darwin/kmccarty-YM2K/default.nix
@@ -6,18 +6,21 @@
   config,
   lib,
   ...
-}: let
+}:
+let
   corretto17 = pkgs.fetchzip {
     name = "corretto17";
     url = "https://corretto.aws/downloads/latest/amazon-corretto-17-aarch64-macos-jdk.tar.gz";
     hash = "sha256-T4eDYeQ3FqQyspa7R0lm1vnC11pNjU2FflV9eh+vPKI=";
   };
-in {
-  launchd.user.envVariables = {
+  sharedEnv = {
     ANDROID_HOME = "/Users/kimberly.mccarty/Library/Android/sdk/";
     JAVA_HOME = "${corretto17}/Contents/Home/";
     GRADLE_LOCAL_JAVA_HOME = "${corretto17}/Contents/Home/";
   };
+in
+{
+  launchd.user.envVariables = sharedEnv;
   # For shell only
   environment.variables = {
     EDITOR = "emacsclient";
@@ -30,5 +33,6 @@ in {
     useGlobalPkgs = true;
     useUserPackages = true;
     backupFileExtension = "backup";
+    users."kimberly.mccarty".home.sessionVariables = sharedEnv;
   };
 }
