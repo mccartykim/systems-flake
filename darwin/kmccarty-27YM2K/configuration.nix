82678d619a019cc977d297c23440f1a1c17f8c6d
Kimberly McCarty <kimberly.mccarty@onepeloton.com>
2026-01-20 12:27:36 -0500

    refactor(kmccarty-27YM2K): merge default.nix into configuration.nix

--- darwin/kmccarty-27YM2K/configuration.nix
+++ darwin/kmccarty-27YM2K/configuration.nix
@@ -1,7 +1,25 @@
-{ pkgs, ... }:
+## Use this file to specify nix-darwin options,
+## which are mostly configuration changes to MacOS. You mayyyy need to rerun
+## `darwin-rebuild switch --flake $THIS_FLAKE` after MacOS updates
 {
-  imports = [ ./default.nix ];
-
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  corretto17 = pkgs.fetchzip {
+    name = "corretto17";
+    url = "https://corretto.aws/downloads/latest/amazon-corretto-17-aarch64-macos-jdk.tar.gz";
+    hash = "sha256-T4eDYeQ3FqQyspa7R0lm1vnC11pNjU2FflV9eh+vPKI=";
+  };
+  sharedEnv = {
+    ANDROID_HOME = "/Users/kimberly.mccarty/Library/Android/sdk/";
+    JAVA_HOME = "${corretto17}/Contents/Home/";
+    GRADLE_LOCAL_JAVA_HOME = "${corretto17}/Contents/Home/";
+  };
+in
+{
   # For some reason, nix-darwin needs this stated explicitly
   users.users."kimberly.mccarty".home = "/Users/kimberly.mccarty";
   users.users."kimberly.mccarty".shell = pkgs.fish;
@@ -88,4 +106,21 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
+
+  # From original default.nix
+  launchd.user.envVariables = sharedEnv;
+  # For shell only
+  environment.variables = {
+    EDITOR = "hx";
+  };
+
+  programs.fish.enable = true;
+  system.defaults.NSGlobalDomain.AppleKeyboardUIMode = 3;
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "backup";
+    users."kimberly.mccarty".home.sessionVariables = sharedEnv;
+  };
 }
