4f050ee6e89092d2588e5fe8efaeb76b2c174673
Kimberly McCarty <kimb@kimb.dev>
2026-03-24 10:38:44 -0400

    fix(maitred): set SANE_CONFIG_DIR in button watcher service

--- hosts/maitred/scanner.nix
+++ hosts/maitred/scanner.nix
@@ -195,6 +195,7 @@ in {
     description = "Fujitsu fi-6130Z scan button watcher";
     after = ["multi-user.target"];
     wantedBy = ["multi-user.target"];
+    environment.SANE_CONFIG_DIR = "/etc/sane-config";
     serviceConfig = {
       ExecStart = buttonWatcher;
       Restart = "always";
