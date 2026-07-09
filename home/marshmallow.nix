29af3305e545a3e0ec7e56daf3aa7c1a75421147
Kimberly McCarty <kimb@kimb.dev>
2026-07-09 17:15:35 -0400

    refactor(marshmallow): drop disabled wayland/sway config rot

--- home/marshmallow.nix
+++ home/marshmallow.nix
@@ -102,81 +102,6 @@
       ];
     };
 
-    swayr = {
-      enable = false;
-      systemd.enable = false;
-      settings = {
-        menu = {
-          executable = "${pkgs.wofi}/bin/wofi";
-          args = [
-            "--show=dmenu"
-            "--allow-markup"
-            "--allow-images"
-            "--insensitive"
-            "--cache-file=/dev/null"
-            "--parse-search"
-            "--height=40%"
-            "--prompt={prompt}"
-          ];
-        };
-
-        format = {
-          output_format = "{indent}<b>Output {name}</b> <span alpha='\"20000\"'>({id})</span>";
-          workspace_format = "{indent}<b>Workspace {name} [{layout}]</b> on output {output_name}    <span alpha='\"20000\"'>({id})</span>";
-          container_format = "{indent}<b>Container [{layout}]</b> <i>{marks}</i> on workspace {workspace_name}    <span alpha='\"20000\"'>({id})</span>";
-          window_format = "img:{app_icon}:text:{indent}<i>{app_name}</i> — {urgency_start}<b>\"{title}\"</b>{urgency_end} <i>{marks}</i> on workspace {workspace_name} / {output_name}    <span alpha='\"20000\"'>({id})</span>";
-          indent = "    ";
-          urgency_start = "<span background='\"darkred\"' foreground='\"yellow\"'>";
-          urgency_end = "</span>";
-          html_escape = true;
-        };
-
-        layout = {
-          auto_tile = false;
-          auto_tile_min_window_width_per_output_width = [
-            [800 400]
-            [1024 500]
-            [1280 600]
-            [1400 680]
-            [1440 700]
-            [1600 780]
-            [1680 780]
-            [1920 920]
-            [2048 980]
-            [2560 1000]
-            [3440 1200]
-            [3840 1280]
-            [4096 1400]
-            [4480 1600]
-            [7680 2400]
-          ];
-        };
-
-        focus = {
-          lockin_delay = 750;
-        };
-
-        misc = {
-          seq_inhibit = false;
-        };
-      };
-    };
-
-    waybar = {
-      enable = false;
-      systemd.enable = false;
-    };
-
-    wofi = {
-      enable = false;
-      settings = {
-        allow_images = true;
-        insensitive = true;
-        allow_markup = true;
-        parse-search = true;
-      };
-    };
-
     swaylock = {
       enable = true;
     };
@@ -185,57 +110,8 @@
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
-  # Wayland configuration
-  wayland.windowManager.sway = {
-    enable = false;
-    systemd.enable = false;
-    config = {
-      modifier = "Mod4";
-      terminal = "kitty";
-      menu = "wofi --show drun,run";
-      bars = [];
-      keybindings = let
-        inherit (config.wayland.windowManager.sway.config) modifier;
-      in
-        lib.mkOptionDefault {
-          "Mod1+Tab" = "exec ${pkgs.swayr}/bin/swayr switch-to-urgent-or-lru-window";
-          #
-          #
-          "${modifier}+Shift+f" = "floating toggle";
-          "${modifier}+Shift+w" = "focus mode_toggle";
-          "${modifier}+c" = "exec ${pkgs.swayr}/bin/swayr execute-swaymsg-command";
-          "${modifier}+Shift+c" = "exec ${pkgs.swayr}/bin/swayr execute-swayr-command";
-          "${modifier}+Tab" = "exec ${pkgs.swayr}/bin/swayr switch-window";
-          "${modifier}+Shift+Tab" = "exec ${pkgs.swayr}/bin/swayr switch-workspace-or-window";
-          "XF86AudioRaiseVolume" = "exec ${pkgs.pamixer}/bin/pamixer -i 5";
-          "XF86AudioLowerVolume" = "exec ${pkgs.pamixer}/bin/pamixer -d 5";
-          "XF86AudioMute" = "exec ${pkgs.pamixer}/bin/pamixer --toggle-mute";
-        };
-    };
-  };
-
   # Services configuration
   services = {
-    swayidle = {
-      enable = false;
-      events = [
-        {
-          event = "before-sleep";
-          command = "${pkgs.swaylock}/bin/swaylock -fF";
-        }
-      ];
-      timeouts = [
-        {
-          timeout = 60 * 5;
-          command = "${pkgs.swaylock}/bin/swaylock -fF";
-        }
-        {
-          timeout = 60 * 20;
-          command = "${pkgs.systemd}/bin/systemctl suspend";
-        }
-      ];
-    };
-
     clipmenu = {
       enable = true;
       launcher = "wofi";
