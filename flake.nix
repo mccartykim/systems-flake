658ba89ee4f7cabffa525609923cb9f13e47950e
Kimberly McCarty <kimb@kimb.dev>
2026-07-29 20:01:22 -0400

    arch(all): switched to nixos-unstable-small channel from nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
     # Add flake-parts
     flake-parts.url = "github:hercules-ci/flake-parts";
